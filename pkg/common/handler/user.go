09b13cde757def8eedbf3b2b97d24ff89da3e208
Paul James Cleary <pauljamescleary@gmail.com>
2023-02-23 16:42:27 -0500

    Got database working right

--- pkg/common/handler/user.go
+++ pkg/common/handler/user.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/pauljamescleary/gomin/pkg/common/models"
 )
@@ -12,6 +13,8 @@ func (h *Handler) CreateUser(c echo.Context) error {
 	if err := c.Bind(u); err != nil {
 		return err
 	}
+
+	u.ID = uuid.New()
 	newUser, err := h.UserRepo.CreateUser(u)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
